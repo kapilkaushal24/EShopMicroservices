a518ad5e6ee1e14c614ccad9966e36d17c6275e8
kapilkauushal24 <kapilkaushal466@gmail.com>
2025-12-15 23:05:51 +0530

    Basket API Microservice Done

---
+++ src/Services/Basket/Basket.API/Exception/BasketNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.API.Exception
+{
+    public class BasketNotFoundException : NotFoundException
+    {
+        public BasketNotFoundException(string userName) : base("Basket", userName)
+        {
+            
+        }
+    }
+}
