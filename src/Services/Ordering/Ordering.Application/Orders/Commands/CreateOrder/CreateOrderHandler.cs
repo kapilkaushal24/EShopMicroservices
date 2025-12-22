08ac24824bae217b9cf2f84c52d1b9df8d43cc6c
kapilkauushal24 <kapilkaushal466@gmail.com>
2025-12-22 21:00:51 +0530

    Add CQRS order creation and application-layer DTOs
    Introduce DTOs for orders, address, items, and payment. Add IApplicationDbContext interface and update ApplicationDbContext to implement it. Register IApplicationDbContext for DI. Scaffold CreateOrderCommand and handler with validation. Add EF Core package and global usings for CQRS and FluentValidation. Update project structure with new folders.

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,19 @@
+﻿
+using Ordering.Application.Data;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderHandler(IApplicationDbContext dbContext)
+        : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            //create Order entity from command object
+            //save to database
+            //return result 
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
